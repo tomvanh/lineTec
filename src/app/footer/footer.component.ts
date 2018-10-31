bdc079d113691e3f9b10e3f5e77c4337a2c3d31a
Tom Van Huffel <tom@alpha-it.be>
2018-10-31 16:38:26 +0100

    footer en header update

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  info = {
+    street: 'Vrijheidstraat',
+    number: '30',
+    postal: '9140',
+    city: 'Temse',
+    email: 'info@hubbusinesscenter.be',
+    telephone: '+32485558588'
+  };
 
   constructor() { }
 
